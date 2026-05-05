251d8a7da04bca41cf99fd7f986afa1ca25a07b8
soncine <soncineenzo@gmail.com>
2026-05-04 21:11:30 -0300

    adicionei um Service para o springboot gerenciar o impCatSer e passar as informações adiante

--- src/main/java/com/ecommerce/soncineStore/service/implementCategoryService.java
+++ src/main/java/com/ecommerce/soncineStore/service/implementCategoryService.java
@@ -1,10 +1,12 @@
 package com.ecommerce.soncineStore.service;
 
 import com.ecommerce.soncineStore.model.Category;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class implementCategoryService implements categoryService {
 
     private List<Category> categories = new ArrayList<>();
